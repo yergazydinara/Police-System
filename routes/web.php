9e15c5ce75328dff64e3f28f2a27946ebd853d42
Batyrkhan Shakenov <batr1996@gmail.com>
2018-10-29 21:35:59 +0600

    Route: fix all routes.

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () { return view('welcome'); });
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
